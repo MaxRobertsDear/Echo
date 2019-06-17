8304a02b2ad680905c4f7f849376dfca1236361b
MaxRobertsDear <maxhroberts@gmail.com>
2019-06-17 22:07:03 +0100

    Cleanup

---
+++ spec/EchoSpec.js
@@ -0,0 +1,18 @@
+describe("E ho", function() {
+  var Echo = require('../src/Echo');
+  var echo;
+
+  beforeEach(function() {
+    echo = new Echo();
+  });
+
+  
+  
+});
+    // it("should be able to play a Song", function() {
+    //   player.play(song);
+    //   expect(player.currentlyPlayingSong).toEqual(song);
+  
+    //   //demonstrates use of custom matcher
+    //   expect(player).toBePlaying(song);
+    // });
---
+++ src/Echo.js
@@ -0,0 +1,10 @@
+function Echo() {
+}
+
+
+
+module.exports = Echo();
+  // Player.prototype.play = function(song) {
+  //   this.currentlyPlayingSong = song;
+  //   this.isPlaying = true;
+  // };
